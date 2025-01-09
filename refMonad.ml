494b15f737c075bcb582afe74477aab6af98fb84
s_feles_ <lukas.janicki@poczta.fm>
2025-01-10 00:19:35 +0100

    Implemented backtracking in the state monad.
    This version of the backtracking monad supports state recovery across branches - each calculation in the monadic Seq.t is paired
    to its own state, each individually updated as the backtracking continues. This should allow progress towards correct Prolog
    interpretation.

--- refMonad.ml
+++ refMonad.ml
@@ -4,30 +4,35 @@ module RefMonad (Value : sig type t end)  (Key : Map.OrderedType) : sig
 
   val return : 'a -> 'a t
   val bind : 'a t -> ('a -> 'b t) -> 'b t
-
   val new_ref : Value.t -> key_ref -> key_ref t
   val get : key_ref -> Value.t option t
   val set : key_ref -> Value.t -> unit t
-  val run : 'a t -> 'a
+  val fail : 'a t
+  val flip : bool t
+  val run : 'a t -> 'a Seq.t
 end with type key_ref = Key.t = struct
   module KeyMap = Map.Make(Key)
 
-  type 'a t = Value.t KeyMap.t -> 'a *  Value.t KeyMap.t
+  type 'a t = Value.t KeyMap.t -> ('a *  Value.t KeyMap.t) Seq.t
   type key_ref = Key.t
 
-  let return x = fun s -> (x, s)
-  let bind m f = fun s ->
-    let (a, s) = m s in
-    f a s
+  let return x = fun st -> Seq.cons (x, st) Seq.empty
+
+  let bind m f = fun st ->
+    Seq.concat_map (fun (x, st) -> f x st) (m st)
+
+  let fail = fun _ -> Seq.empty
+
+  let flip = fun st -> List.to_seq [ true, st; false, st ]
   
   let new_ref (v : Value.t) (k : key_ref) = fun s ->
-    (k, KeyMap.add k v s)
+    Seq.cons (k, KeyMap.add k v s) Seq.empty
   
   let get r = fun s ->
-    (KeyMap.find_opt r s, s)
+    Seq.cons (KeyMap.find_opt r s, s) Seq.empty
   
   let set r v = fun s ->
-    ((), KeyMap.add r v s)
+    Seq.cons ((), KeyMap.add r v s) Seq.empty
   
-  let run m = fst (m KeyMap.empty)
+  let run m = Seq.map fst (m KeyMap.empty)
 end
