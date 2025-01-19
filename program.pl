658377bde9bda31b5df4ee7cb2ecf48cd0888363
s_feles_ <lukas.janicki@poczta.fm>
2025-01-19 17:08:52 +0100

    Rewrote example program in list notation

--- program.pl
+++ program.pl
@@ -4,8 +4,8 @@ select(X, [H|T], [H|R]) :- select(X, T, R).
 perm([], []).
 perm(Xs, [Y|Zs]) :- select(Y, Xs, Ys), perm(Ys, Zs).
 
-append(nil, Xs, Xs).
-append(cons(H, Xs), Ys, cons(H, Zs)) :- append(Xs, Ys, Zs).
+append([], Xs, Xs).
+append([H|Xs], Ys, [H|Zs]) :- append(Xs, Ys, Zs).
 
 len([], 0).
 len([_|Xs], N) :- len(Xs, M), N is M+1.
